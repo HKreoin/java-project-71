206b45b34799d73f115b02ffb7a59f5709fa4392
HKreoin <steamuser85@yandex.ru>
2024-04-28 11:09:22 +0300

    modify DIffer class for understandable

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -13,36 +13,36 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 class Differ {
 
-    public static Map<String, Object> getData(String content) throws Exception {
-        Path p = Paths.get(content);
+    public static Map<String, Object> getData(String filePath) throws Exception {
+        Path p = Paths.get(filePath);
         ObjectMapper objectMapper = new ObjectMapper();
-        var result = objectMapper.readValue(Files.readString(p), new TypeReference<Map<String, Object>>(){});
-        return result;
+        var file = objectMapper.readValue(Files.readString(p), new TypeReference<Map<String, Object>>(){});
+        return file;
     }
 
-    public static String generate(Map<String, Object> map1, Map<String, Object> map2){
+    public static String generate(Map<String, Object> file1, Map<String, Object> file2){
         var treeMap = new TreeMap<String, Object>();
         List<String> list = new ArrayList<>(); 
-        treeMap.putAll(map1);
-        treeMap.putAll(map2);
+        treeMap.putAll(file1);
+        treeMap.putAll(file2);
 
         treeMap.forEach((k, v) -> {
-            if (map1.containsKey(k) && map2.containsKey(k)) {
-                if (map1.get(k).equals(map2.get(k))) {
+            if (file1.containsKey(k) && file2.containsKey(k)) {
+                if (file1.get(k).equals(file2.get(k))) {
                     list.add("    " + k + ": " + v);
                 } else {
-                    list.add("  - " + k + ": " + map1.get(k));
+                    list.add("  - " + k + ": " + file1.get(k));
                     list.add("  + " + k + ": " + v);
                 }
-            } else if (map1.containsKey(k)) {
-                list.add("  - " + k + ": " + map1.get(k));
-            } else if (map2.containsKey(k)) {
+            } else if (file1.containsKey(k)) {
+                list.add("  - " + k + ": " + file1.get(k));
+            } else if (file2.containsKey(k)) {
                 list.add("  + " + k + ": " + v);
             }
         });
         list.add(0, "{");
         list.add("}");
-        String result = String.join("\n", list);
-        return result;
+        String difference = String.join("\n", list);
+        return difference;
     }
 }
