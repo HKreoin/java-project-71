69702f9a00d190f9b7cc4afc5378925de91d505a
Kreoin <steamuser85@yandex.ru>
2024-05-22 00:40:33 +0300

    linter format class

--- src/main/java/hexlet/code/Format.java
+++ src/main/java/hexlet/code/Format.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 class Format {
 
-    public static String reflect(Map <String, Object> map, String format) {
+    public static String reflect(Map<String, Object> map, String format) {
         return switch (format) {
             case "stylish" -> stylish(map);
             default -> throw new IllegalArgumentException("Unexpected value: " + format);
