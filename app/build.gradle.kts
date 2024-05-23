0f44f70c3a534a45506e3a3a79258cd5efd059b2
HKreoin <steamuser85@yandex.ru>
2024-05-23 22:23:10 +0300

    add checkstyle verion to build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,3 +54,7 @@ tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
     reports { xml.required.set(true) }
 }
+
+checkstyle {
+    toolVersion '9.2.1'
+}
