2409c8bd5f19190695d9e2ef348fd6b67a824f75
Kreoin <steamuser85@yandex.ru>
2024-06-02 18:17:47 +0300

    modify build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,38 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
+    id("java")
     application
-    jacoco
     id("checkstyle")
-    id("io.freefair.lombok") version "8.6"
-    id("com.adarshr.test-logger") version "4.0.0"
-    id("com.github.ben-manes.versions") version "0.51.0"
+    jacoco
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // This dependency is used by the application.
-    implementation(libs.guava)
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.5")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
 }
 
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use JUnit Jupiter test framework
-            useJUnitJupiter("5.10.0")
-        }
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
     }
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
-application {
-    // Define the main class for the application.
-    mainClass.set("hexlet.code.App")
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
-    reports { xml.required.set(true) }
-}
-
-
-checkstyle {
-    toolVersion = "10.12.4"
-}
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
